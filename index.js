2fde3aa2851d8fa02ecded0f6c8e45b497100ee1
rafaulgoni <rafaulgoni787898@gmail.com>
2024-05-13 03:48:19 +0600

    add more crud

--- index.js
+++ index.js
@@ -87,6 +87,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/card/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      const updateDoc = {
+          $set: {
+              status: updatedBooking.status
+          },
+      };
+      const result = await cardCollection.updateOne(filter, updateDoc);
+      res.send(result);
+  })
+
     app.delete('/card/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
